3b4d24ec38c8b7efc09d7f6f30b2937fcf90f763
truptijadh <86972126+truptijadh@users.noreply.github.com>
2021-07-13 23:00:41 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,211 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var playerMallet;
+
+
+var boundary1 = createSprite(200,0,400,10);
+boundary1.shapeColor = "white";
+var boundary2 = createSprite(200,400,400,10);
+boundary2.shapeColor = "white";
+var boundary3 = createSprite(0,200,10,400);
+boundary3.shapeColor = "white";
+var boundary4 = createSprite(400,200,10,400);
+boundary4.shapeColor = "white";
+var boundary5=createSprite(200,10,400,5);
+boundary5.shapeColor="white";
+var boundary6=createSprite(200,390,400,5);
+boundary6.shapeColor="white";
+ 
+//making court
+var goal1=createSprite(200,18,100,20);
+goal1.shapeColor=("yellow");
+
+var goal2=createSprite(200,382,100,20);
+goal2.shapeColor=("yellow");
+
+
+// creating objects and giving them colours
+var striker = createSprite(200,200,10,10);
+striker.shapeColor = "white";
+
+var playerMallet = createSprite(200,50,50,10);
+playerMallet.shapeColor = "black";
+
+var computerMallet = createSprite(200,350,50,10);
+computerMallet.shapeColor = "black";
+
+var compscore=0;
+var playerscore=0;
+var gameState="serve";
+function draw() {
+  //clear the screen
+  background("green");
+
+
+  if (gameState=="serve") {
+   // display text
+   textSize(18);
+   fill("maroon");
+text("press space to strike",120,180);
+  }
+   textSize(18);
+   fill("maroon");
+  text(compscore,25,225);
+text(playerscore,25,185);
+
+  //serve the striker when space is pressed
+  if (keyDown("space")) {
+    serve();
+    gameState="play"
+  }
+  
+ if (striker.isTouching(goal1)) {
+  compscore=compscore+1
+  striker.velocityX=0;
+  striker.velocityY=0;
+  striker.x=200;
+  striker.y=200;
+}
+  if (striker.isTouching(goal2)) {
+  playerscore=playerscore=+1
+  
+  striker.velocityX=0;
+  striker.velocityY=0;
+  striker.x=200;
+  striker.y=200;
+}  
+if (playerscore==5||compscore==5) {
+  
+  fill("maroon");
+  textSize(18);
+  text("Gameover!,170,160");}
+   
+  
+  
+  //make the player paddle move with the Arrow keys
+  paddleMovement();
+ 
+
+
+  
+  //AI for the computer paddle
+  //make it move with the striker's y position
+  computerMallet.x = striker.x;
+
+  
+  //draw line at the centre
+   for (var i = 0; i < 400; i=i+20) {
+    line(i,200,i+10,200);
+  }
+  
+  //create edge boundaries
+  //make the striker bounce with the top and the bottom edges
+  createEdgeSprites();
+  
+  striker.bounceOff(edges);
+  striker.bounceOff(playerMallet);
+  striker.bounceOff(computerMallet);
+  
+  playerMallet.bounceOff(edges);
+  computerMallet.bounceOff(edges);
+  
+
+ 
+  
+  
+  
+ drawSprites();
+  
+}
+
+
+ function paddleMovement()
+{
+  if(keyDown("left")){
+    playerMallet.x = playerMallet.x-10;
+    
+  }
+  
+  if(keyDown("right")){
+    playerMallet.x = playerMallet.x+10;
+    
+  }
+  
+  if(keyDown("up")){
+   if(playerMallet.y>25)
+   {
+    playerMallet.y = playerMallet.y- 10;
+   }
+  }
+  
+  if(keyDown("down")){
+    if(playerMallet.y<120)
+   {
+    playerMallet.y = playerMallet.y+10;
+   }
+  }
+}
+
+
+
+function serve() {
+  striker.velocityX = 10;
+  striker.velocityY = 5;
+  
+ 
+}
+
+
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
